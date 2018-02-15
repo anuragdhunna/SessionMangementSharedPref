3d4cef6dd99f428f9d004e59a795853ad019773b
Anurag <anurag.dhunna@viithiisys.com>
2018-02-15 17:36:00 +0530

    name changed

--- app/src/main/java/com/anuragdhunna/www/sessionmangementsharedpref/activities/LoginActivity.java
+++ app/src/main/java/com/anuragdhunna/www/sessionmangementsharedpref/activities/LoginActivity.java
@@ -32,7 +32,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         username = findViewById(R.id.usernameText);
         password = findViewById(R.id.passwordText);
